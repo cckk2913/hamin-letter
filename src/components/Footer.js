36ea5b38b567a1ecb1df9c2dd27aa3588b7f05a3
Pin-Er Chen <cckk2913@gmail.com>
2024-10-31 16:44:19 +0800

    css for mobile

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -10,12 +10,14 @@ export const Footer = () => {
       <Container>
         <Row className="align-items-center">
           
-          <Col size={12} sm={6}>
-           
+          <Col size={12} sm={6} className="text-center text-sm-start">
+          
+          <p>All rights reserved by PLAVE and VLAST Co., Ltd.</p>
           </Col>
           <Col size={12} sm={6} className="text-center text-sm-end">
-            <p>Built by Una (Plurk: @cckk2913)</p>
-            <p>All Rights Reserved by VLAST Co., Ltd.</p>
+          <p>Created by Una (Plurk: @cckk2913)</p>
+            {/* <p>This page is for record-keeping purposes only</p> */}
+
           </Col>
         </Row>
       </Container>
